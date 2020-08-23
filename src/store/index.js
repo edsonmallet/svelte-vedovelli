7830ca161115248ee6e51536b8a401d9a162b4f4
Edson Mallet <edsonmmr@gmail.com>
2020-08-22 21:39:49 -0300

    feat: movies search, card e list components

---
+++ src/store/index.js
@@ -0,0 +1,17 @@
+import { writable, derived } from 'svelte/store'
+
+export const INITIAL_STATE = {
+    movies: [],
+    wasSearched: false
+}
+
+export const store = writable({ ...INITIAL_STATE })
+
+export const movies = derived(store, (store) => {
+    return store.movies
+        .filter((movie) => movie.backdrop_patch != null)
+        .map((movie) => {
+            movie.friendly_date = new Date(movie.release_date).toLocaleDateString()
+            return movie
+        })
+})
